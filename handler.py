6507bef9c90201ac2543d1df4f5777511863f630
a.mametyev <mametevalex@gmail.com>
2025-11-21 14:53:36 +0100

    blac

--- handler.py
+++ handler.py
@@ -4,30 +4,29 @@ import runpod
 import torch
 import peft
 import time
-import transformers
-import diffusers
 from PIL import Image
 import io
 from diffusers import QwenImageEditPipeline
 from huggingface_hub import hf_hub_download
 import sys
 from loguru import logger
+
 logger.debug(sys.executable)
 
 pipeline = QwenImageEditPipeline.from_pretrained("Qwen/Qwen-Image-Edit")
 pipeline = pipeline.to(torch.bfloat16)
-logger.debug('Qwen downloaded')
+logger.debug("Qwen downloaded")
 
 lora_path_4_step = hf_hub_download(
     repo_id="lightx2v/Qwen-Image-Lightning",
-    filename="Qwen-Image-Edit-Lightning-4steps-V1.0-bf16.safetensors"
+    filename="Qwen-Image-Edit-Lightning-4steps-V1.0-bf16.safetensors",
 )
-logger.debug('Lora 1 downloaded')
+logger.debug("Lora 1 downloaded")
 lora_path_nsfw = hf_hub_download(
     repo_id="lightx2v/Qwen-Image-Lightning",
-    filename="Qwen-Image-Edit-Lightning-4steps-V1.0-bf16.safetensors"
+    filename="Qwen-Image-Edit-Lightning-4steps-V1.0-bf16.safetensors",
 )
-logger.debug('Lora 2 downloaded')
+logger.debug("Lora 2 downloaded")
 pipeline.load_lora_weights(lora_path_4_step, adapter_name="steps")
 pipeline.load_lora_weights(lora_path_nsfw, adapter_name="nsfw")
 pipeline.set_adapters(["steps", "nsfw"], adapter_weights=[1.0, 1.0])
@@ -35,6 +34,7 @@ logger.debug("Lora downloaded")
 
 pipeline.fuse_lora()
 logger.debug("Lora Fused")
+
 
 def handler(job):
     """
@@ -53,11 +53,12 @@ def handler(job):
         image=image,
         prompt=job_input["prompt"],
         num_inference_steps=job_input.get("num_inference_steps", 4),
-        generator=torch.manual_seed(10**4)
+        generator=torch.manual_seed(10**4),
     )
     logger.info(f"Image generated. Time taken: {time.time() - t0}")
     output_image = output.images[0]
     output_image_base64 = base64.b64encode(output_image.tobytes()).decode("ascii")
     return {"image_base64": output_image_base64}
+
 
 runpod.serverless.start({"handler": handler})
